d03d79e336eaccf4ddc1f1ad990549dbfcb2f18a
Pixel <youssefiyd10@gmail.com>
2024-04-20 08:47:58 +0300

    Basic Layout to switch between sensors - Added Main Screen, PushUpScreen, SitUpsScreen and Walking Step Sensor Screens - Added Buttons on main to navigate to each screen - Added back button for each screen to navigate back to main - Added Title of each screen and associate with relevant sensor

--- app/src/main/java/com/example/strongestmanalive/ui/main/mainScreen.kt
+++ app/src/main/java/com/example/strongestmanalive/ui/main/mainScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,16 +24,28 @@ import android.Manifest
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.saveable.rememberSaveable
 import com.example.strongestmanalive.R
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.permissions.shouldShowRationale
 
+enum class Screens{
+    MAIN, PUSHUP, SITUP, WALKING
+}
+
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MainScreen(mainViewModel: MainViewModel = viewModel(factory = MainViewModel.factory)) {
+    var currentScreen by rememberSaveable {
+        mutableStateOf(Screens.MAIN)
+    }
     val numberOfPushUps = mainViewModel.numberOfPushUps.collectAsState()
     val numberOfSitUps = mainViewModel.numberOfBurpees.collectAsState()
     val numberOfSteps = mainViewModel.numberOfSteps.collectAsState()
@@ -60,28 +71,160 @@ fun MainScreen(mainViewModel: MainViewModel = viewModel(factory = MainViewModel.
             requestPermissionLauncher.launch(Manifest.permission.ACTIVITY_RECOGNITION)
         }
     }
-    var isStarted by remember {
+
+
+
+    when(currentScreen){
+        Screens.MAIN -> MainScreenM(onSitUpPress = { currentScreen = Screens.SITUP }, onPushUpPress = { currentScreen = Screens.PUSHUP }) {
+            currentScreen = Screens.WALKING
+        }
+        Screens.PUSHUP -> PushUpScreen(
+            numberOfPushUps = numberOfPushUps.value,
+            startExercise = { mainViewModel.startAccelSensor() },
+            stopExercise = { mainViewModel.stopAccelerationSensor() },
+            onBackPress = {currentScreen = Screens.MAIN})
+        Screens.SITUP -> SitUpScreen(
+            numberOfSitUps = numberOfSitUps.value,
+            startExercise = { mainViewModel.startAccelSensorBurpee() },
+            stopExercise = { mainViewModel.stopAccelerationSensorBurpee() },
+            onBackPress = {currentScreen = Screens.MAIN}
+        )
+        Screens.WALKING -> WalkingScreen(
+            numberOfSteps = numberOfSteps.value,
+            startExercise = { mainViewModel.startAccelSensorBurpee() },
+            stopExercise = { mainViewModel.stopAccelerationSensorBurpee() },
+            onBackPress = {currentScreen = Screens.MAIN}
+        )
+    }
+
+}
+
+@Composable
+fun MainScreenM(onSitUpPress: () -> Unit, onPushUpPress: () -> Unit, onWalkingPress: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+        Text(text = "Start Your Favorite Exercise Now", Modifier.padding(horizontal = 4.dp, vertical = 16.dp))
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(horizontalArrangement = Arrangement.SpaceAround) {
+                Button(modifier = Modifier.padding(4.dp), onClick = { onPushUpPress() }) {
+                    Text(text = "PUSH-UPS")
+                }
+                Button(modifier = Modifier.padding(4.dp), onClick = { onSitUpPress() }) {
+                    Text(text = "SIT-UPS")
+                }
+                Button(modifier = Modifier.padding(4.dp), onClick = { onWalkingPress() }) {
+                    Text(text = "WALKING")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PushUpScreen(numberOfPushUps: Int, startExercise: () -> Unit, stopExercise: () -> Unit, onBackPress: () -> Unit) {
+    var isExerciseStarted by rememberSaveable {
         mutableStateOf(false)
     }
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Text(text = numberOfSitUps.value.toString(), fontSize = 42.sp)
-        Spacer(modifier = Modifier.size(65.dp))
-        Image(painter = if(!isStarted){painterResource(id = R.drawable.baseline_play_arrow_24)}else{
-            painterResource(id = R.drawable.baseline_pause_24)}, contentDescription = "StartStop", modifier = Modifier
-            .size(60.dp)
-            .clickable {
-                isStarted = if (isStarted) {
-                    mainViewModel.stopAccelerationSensorBurpee()
-                    false
-                } else {
-                    mainViewModel.startAccelSensorBurpee()
-                    true
-                }
-            })
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp, horizontal = 4.dp), contentAlignment = Alignment.CenterStart){
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+                Text(text = "PUSH UP")
+            }
+            Image(painter = painterResource(id = R.drawable.baseline_arrow_back_24), contentDescription = "Back ARROW", modifier = Modifier.clickable { onBackPress() })
+
+        }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text(text = numberOfPushUps.toString(), fontSize = 42.sp)
+            Spacer(modifier = Modifier.size(65.dp))
+            Image(painter = if(!isExerciseStarted){painterResource(id = R.drawable.baseline_play_arrow_24)}else{
+                painterResource(id = R.drawable.baseline_pause_24)}, contentDescription = "StartStop", modifier = Modifier
+                .size(60.dp)
+                .clickable {
+                    isExerciseStarted = if (isExerciseStarted) {
+                        stopExercise()
+                        false
+                    } else {
+                        startExercise()
+                        true
+                    }
+                })
+        }
+    }
+
+}
+
+@Composable
+fun SitUpScreen(numberOfSitUps: Int, startExercise: () -> Unit, stopExercise: () -> Unit, onBackPress: () -> Unit) {
+    var isExerciseStarted by rememberSaveable {
+        mutableStateOf(false)
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp, horizontal = 4.dp), contentAlignment = Alignment.CenterStart){
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+                Text(text = "SIT UP")
+            }
+            Image(painter = painterResource(id = R.drawable.baseline_arrow_back_24), contentDescription = "Back ARROW", modifier = Modifier.clickable { onBackPress() })
+
+        }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text(text = numberOfSitUps.toString(), fontSize = 42.sp)
+            Spacer(modifier = Modifier.size(65.dp))
+            Image(painter = if(!isExerciseStarted){painterResource(id = R.drawable.baseline_play_arrow_24)}else{
+                painterResource(id = R.drawable.baseline_pause_24)}, contentDescription = "StartStop", modifier = Modifier
+                .size(60.dp)
+                .clickable {
+                    isExerciseStarted = if (isExerciseStarted) {
+                        stopExercise()
+                        false
+                    } else {
+                        startExercise()
+                        true
+                    }
+                })
+        }
+    }
+}
+
+@Composable
+fun WalkingScreen(numberOfSteps: Int, startExercise: () -> Unit, stopExercise: () -> Unit, onBackPress: () -> Unit) {
+    var isExerciseStarted by rememberSaveable {
+        mutableStateOf(false)
+    }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxWidth().padding(vertical = 12.dp, horizontal = 4.dp), contentAlignment = Alignment.CenterStart){
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+                Text(text = "Walking Step Counter")
+            }
+            Image(painter = painterResource(id = R.drawable.baseline_arrow_back_24), contentDescription = "Back ARROW", modifier = Modifier.clickable { onBackPress() })
+
+        }
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text(text = numberOfSteps.toString(), fontSize = 42.sp)
+            Spacer(modifier = Modifier.size(65.dp))
+            Image(painter = if(!isExerciseStarted){painterResource(id = R.drawable.baseline_play_arrow_24)}else{
+                painterResource(id = R.drawable.baseline_pause_24)}, contentDescription = "StartStop", modifier = Modifier
+                .size(60.dp)
+                .clickable {
+                    isExerciseStarted = if (isExerciseStarted) {
+                        stopExercise()
+                        false
+                    } else {
+                        startExercise()
+                        true
+                    }
+                })
+        }
     }
 }
 
